371427792d6026a857482b1d47a75f82bc908ba2
DenzelMwangi <159570611+LofftyPixel@users.noreply.github.com>
2025-09-14 16:59:59 +0300

    Add files via upload

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,9 @@
-#!/usr/bin/node
-
 const args = process.argv.slice(2);
 
-console.log(args[0] || "undefined");
-console.log(args[1] || "undefined");
-console.log(args[2] || "undefined");
-console.log(args[3] || "undefined");
+if (args.length ===0 ){
+    console.log('No argument');
+} else if (args.length===1){
+    console.log('Argument found');
+} else {
+    console.log('Argument found')
+}
