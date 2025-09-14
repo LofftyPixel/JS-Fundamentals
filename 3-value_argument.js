65442c1a155827a7c6a3ccd523634213648eb2f7
DenzelMwangi <159570611+LofftyPixel@users.noreply.github.com>
2025-09-14 17:36:58 +0300

    Add files via upload

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,6 +2,8 @@ console.log=Process .args.slice(2);
 
 if (args<1){
     console.log('No Argument');
-}else if (args===1){
-    console.log('Argument found');
+}else if (args>=1){
+    console.log('HBTN');
+}else{
+    console.log('HBTN cool')
 }
