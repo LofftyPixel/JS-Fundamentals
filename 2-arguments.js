1dad0f691e85c644b0922cdc3a19941fb4de13e0
DenzelMwangi <159570611+LofftyPixel@users.noreply.github.com>
2025-09-14 17:12:18 +0300

    Add files via upload

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,17 @@
 const args = process.argv.slice(2);
 
-if (args.length ===0 ){
-    console.log('No argument');
-} else if (args.length===1){
-    console.log('Argument found');
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Argument found')
+  console.log('Arguments found');
 }
+// Another way to do it
+// if (args.length === 0) {
+//   console.log('No argument');
+// } else if (args.length === 1) {
+//   console.log('Argument found');
+// } else {
+//   console.log('Arguments found');
+// }
